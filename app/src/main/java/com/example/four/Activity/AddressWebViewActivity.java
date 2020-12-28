ebe38034c923bc4bc94de0314cb5892bde0670b7
ram <3017129@naver.com>
2020-12-28 17:57:30 +0900

    로그인 합침, 서치 xml 추가

--- app/src/main/java/com/example/four/Activity/AddressWebViewActivity.java
+++ app/src/main/java/com/example/four/Activity/AddressWebViewActivity.java
@@ -50,6 +50,6 @@ public class AddressWebViewActivity extends AppCompatActivity {
             }
         });
 
-        browser.loadUrl("http://192.168.35.147:8080/test/daum.html");
+        browser.loadUrl("http://192.168.0.105:8080/test/daum.html"); //여기 자기 주소로 바꾸기
     }
 }
